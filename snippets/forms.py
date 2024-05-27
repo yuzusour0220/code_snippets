d16ee4b311112209f0ff6529b2c460337893654e
Takumi Sakai <miniminiuou@gmail.com>
2024-05-27 14:17:22 +0900

    first_commit

---
+++ snippets/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from snippets.models import Snippet
+class SnippetForm(forms.ModelForm):
+    class Meta:
+        model = Snippet
+        fields = ("title", "code", "description")
